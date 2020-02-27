438ef5aa06734b4989fc4ef1ee105bc39625c106
wangxin <443744345@qq.com>
2020-02-27 22:48:58 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 // fastClick解决移动端300ms延迟问题
 import fastClick from 'fastclick'
+// swiper轮播
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 // 加载重置css
 import '@css/reset.css'
 // 1像素边框问题解决
@@ -11,9 +14,10 @@ import '@css/border.css'
 // 引入iconfont
 import '@css/iconfont/iconfont.css'
 
-
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 new Vue({
   router,
   store,
---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination"
+        }
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl: require("@/assets/img/banner1.jpg")
+        },
+        {
+          id: "0002",
+          imgUrl: require("@/assets/img/banner2.jpg")
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper ::v-deep .swiper-pagination-bullet-active {
+  background: #fff;
+}
+.wrapper {
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  overflow: hidden;
+}
+.swiper-img {
+  width: 100%;
+}
+</style>
