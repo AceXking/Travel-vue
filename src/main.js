c6067b91379743fe3abf1c63d6868f685e0a9e52
wangxin <443744345@qq.com>
2020-02-27 18:22:25 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// fastClick解决移动端300ms延迟问题
+import fastClick from 'fastclick'
+// 加载重置css
+import './assets/css/reset.css'
+// 1像素边框问题解决
+import './assets/css/border.css'
+
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 new Vue({
   router,
   store,
